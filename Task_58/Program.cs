86857db215b08c345b5de8642018b8ab8770089c
Anton <kiberkan@gmail.com>
2023-05-22 20:48:03 +0700

    it's decided

---
+++ Task_58/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+int[,] CreatinRandomArray(int minValue = 1, int maxValue = 9, int rows = 2, int columns = 2)
+{
+    int[,] arr = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            arr[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+
+int [,] firstMartrix = CreatinRandomArray();
+int [,] secomdMartrix = CreatinRandomArray();
+int [,] resultMatrix = new int [2,2];
+PrintArray(firstMartrix);
+PrintArray(secomdMartrix);
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+PrintArray(resultMatrix);
+
